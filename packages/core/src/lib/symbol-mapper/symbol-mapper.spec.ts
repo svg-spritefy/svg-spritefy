0205c50813fe13f9b7109085537637314cea97a8
Pavel Rybakin <pvrybakin@gmail.com>
2024-09-29 01:27:50 +0300

    test(core): add unit tests for SymbolMapper class

---
+++ packages/core/src/lib/symbol-mapper/symbol-mapper.spec.ts
@@ -0,0 +1,42 @@
+import { SymbolMapper } from './symbol-mapper'
+import { describe, expect, it } from 'vitest'
+
+describe('SymbolMapper class', () => {
+	let symbolMapper: SymbolMapper
+
+	beforeEach(() => {
+		symbolMapper = new SymbolMapper()
+	})
+
+	it('adds a new symbol correctly', () => {
+		const name = 'example'
+		const svg = '<svg></svg>'
+
+		symbolMapper.add(name, svg)
+		expect(symbolMapper.names).toContain(name)
+	})
+
+	it('deletes a symbol correctly', () => {
+		const name = 'example'
+		const svg = '<svg></svg>'
+
+		symbolMapper.add(name, svg)
+		symbolMapper.delete(name)
+
+		expect(symbolMapper.names).not.toContain(name)
+	})
+
+	it('produces a correct string representation', () => {
+		const name = 'example'
+		const svg = '<svg><path d="M0 0h100v100H0z"/></svg>'
+		const expected = '<symbol id="example"><path d="M0 0h100v100H0z"/></symbol>'
+
+		symbolMapper.add(name, svg)
+		const result = symbolMapper.toString()
+		expect(result).toBe(expected)
+	})
+
+	afterEach(() => {
+		symbolMapper = new SymbolMapper()
+	})
+})
