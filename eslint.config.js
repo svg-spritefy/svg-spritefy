1a162376bc35f0f7dd4571e526f4d93cecb527d5
Pavel Rybakin <pvrybakin@gmail.com>
2024-09-28 18:31:38 +0300

    chore: update ESLint and Prettier configuration

--- eslint.config.js
+++ eslint.config.js
@@ -1,33 +1,49 @@
-const nx = require('@nx/eslint-plugin');
+const nx = require('@nx/eslint-plugin')
 
 module.exports = [
-  ...nx.configs['flat/base'],
-  ...nx.configs['flat/typescript'],
-  ...nx.configs['flat/javascript'],
-  {
-    ignores: ['**/dist'],
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    rules: {
-      '@nx/enforce-module-boundaries': [
-        'error',
-        {
-          enforceBuildableLibDependency: true,
-          allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
-          depConstraints: [
-            {
-              sourceTag: '*',
-              onlyDependOnLibsWithTags: ['*'],
-            },
-          ],
-        },
-      ],
-    },
-  },
-  {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
-  },
-];
+	...nx.configs['flat/base'],
+	...nx.configs['flat/typescript'],
+	...nx.configs['flat/javascript'],
+	{
+		ignores: ['**/dist'],
+	},
+	{
+		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+		rules: {
+			'@nx/enforce-module-boundaries': [
+				'error',
+				{
+					enforceBuildableLibDependency: true,
+					allow: ['^.*/eslint(\\.base)?\\.config\\.[cm]?js$'],
+					depConstraints: [
+						{
+							sourceTag: '*',
+							onlyDependOnLibsWithTags: ['*'],
+						},
+					],
+				},
+			],
+		},
+	},
+	{
+		files: ['*.ts', '*.tsx'],
+		extends: ['plugin:@nx/typescript'],
+		rules: {
+			'@typescript-eslint/interface-name-prefix': 'never',
+			'@typescript-eslint/explicit-module-boundary-types': 'off',
+			'@typescript-eslint/no-explicit-any': 'error',
+			'@typescript-eslint/no-empty-function': 'off',
+			'@typescript-eslint/explicit-function-return-type': [
+				'error',
+				{
+					'allowExpressions': true,
+				},
+			],
+		},
+	},
+	{
+		files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
+		// Override or add rules here
+		rules: {},
+	},
+]
